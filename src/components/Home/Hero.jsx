cbc1e1e3f84e3db63c215b9640d842e6652f39c8
Aytech <ayokunumitoluwani00@gmail.com>
2024-03-14 15:36:10 +0100

    init

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
       <div>
         <div className="flex justify-between mb-7">
           <a
-            href="https://dexscreener.com/solana/GWd5e9hbMf5DrpLR8ZhP1gA6CupYgi9mm6D4TLM8wX25"
+            href="https://raydium.io/swap/?inputCurrency=sol&outputCurrency=EuBHfYDXGG1FaGREsJbbP8qMMU7PFAuK1nefYMFbp52f&fixed=in"
             target="_blank"
           >
             <button className="transition-all hover:scale-110 bg-[#FDF8E0] rounded-sm skew-x-6  text-black  text-[14px] md:text-lg px-3 md:px-5 py-1">
@@ -12,7 +12,7 @@ export default function Hero() {
             </button>
           </a>
 
-          <a href="https://t.me/sendor_sol  " target="_blank">
+          <a href="https://twitter.com/Sol_Whitehouse_ " target="_blank">
             <button className=" bg-[#FDF8E0] transition-all hover:scale-110 -skew-x-6 rounded-sm text-black  text-[14px] md:text-lg px-3 md:px-5 py-1">
               TwittER
             </button>
@@ -21,8 +21,7 @@ export default function Hero() {
 
         <div className="text-white flex justify-between">
           <a
-            href="
- https://twitter.com/sendor_sol"
+            href="https://dexscreener.com/solana/EuBHfYDXGG1FaGREsJbbP8qMMU7PFAuK1nefYMFbp52f"
             target="_blank"
           >
             <button className="bg-[#FDF8E0] transition-all hover:scale-110 skew-x-6 rounded-sm text-black  text-[14px] md:text-lg px-3 md:px-5 py-1">
@@ -30,11 +29,7 @@ export default function Hero() {
             </button>
           </a>
 
-          <a
-            href="
- https://twitter.com/sendor_sol"
-            target="_blank"
-          >
+          <a href="https://t.me/WhiteHouse_SOL" target="_blank">
             <button className=" transition-all hover:scale-110 -skew-x-6 rounded-sm bg-[#FDF8E0] text-black  text-[14px] md:text-lg px-3 md:px-5 py-1">
               TeleGRAM
             </button>
